5bc73f5649889a543141570c115de643bad6736a
Tyler Smith <tylerlwsmith@gmail.com>
2021-12-17 16:17:03 -0800

    Moved homepage animation script into JS file.

---
+++ assets/js/homepage-animation.js
@@ -0,0 +1,50 @@
+/**
+ * Animate the homepage text. Must execute immediately: do not move into a
+ * "DOMContentLoaded" callback.
+ */
+(function () {
+  /**
+   * Creating the style tag in JavaScript ensures that the text will be
+   * visible on load if JS is disabled. Placing script before the affected
+   * DOM elements prevents a flash text before the animation starts.
+   */
+  const styleTag = document.createElement("style");
+  styleTag.innerHTML = `
+			.animate\\:fade-in {
+				opacity: 0;
+				transform: translateX(20px);
+				transition-property: opacity, transform;
+				transition-duration: 0.3s;
+			}
+			@media (max-width: 650px) {
+				.animate\\:fade-in {
+					transform: translateY(20px);
+				}
+			}
+			.animate\\:fade-in--initialized {
+				opacity: 1;
+				transform: translateX(0);
+			}
+		`;
+  const headElement = document.querySelector("head");
+  if (headElement) headElement.appendChild(styleTag);
+
+  document.addEventListener(
+    "DOMContentLoaded",
+    function () {
+      window.setTimeout(function () {
+        const animatable = [...document.querySelectorAll(".animate\\:fade-in")];
+        let currentIndex = 0;
+        const interval = window.setInterval(function () {
+          if (currentIndex + 1 >= animatable.length)
+            window.clearInterval(interval);
+          animatable[currentIndex].classList.add(
+            "animate:fade-in--initialized"
+          );
+          currentIndex++;
+        }, 120);
+      }, 100);
+    },
+    false
+  );
+})();
