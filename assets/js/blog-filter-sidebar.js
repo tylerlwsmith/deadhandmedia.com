6714421f25cab20af5e3c4f3b5cceb5872d6b019
Tyler Smith <tylerlwsmith@gmail.com>
2021-12-20 13:50:01 -0800

    Removed unused function
    [skip ci]

--- assets/js/blog-filter-sidebar.js
+++ assets/js/blog-filter-sidebar.js
@@ -45,18 +45,6 @@ window.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Thanks, Josh.
-  // https://www.joshwcomeau.com/snippets/javascript/debounce/
-  function debounce(callback, wait) {
-    let timeoutId = null;
-    return function (...args) {
-      window.clearTimeout(timeoutId);
-      timeoutId = window.setTimeout(() => {
-        callback.apply(null, args);
-      }, wait);
-    };
-  }
-
   function getCheckboxes() {
     return document.querySelectorAll(checkboxSelector);
   }
