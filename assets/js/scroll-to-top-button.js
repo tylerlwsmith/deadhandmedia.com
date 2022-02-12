eb58a3dfcea07c6fdcec3feaded1d1bad2ce9330
Tyler Smith <tylerlwsmith@gmail.com>
2022-02-12 00:08:18 -0800

    Animate scroll-to-top button

--- assets/js/scroll-to-top-button.js
+++ assets/js/scroll-to-top-button.js
@@ -1,12 +1,20 @@
 import { unloadEvent } from "./event-names";
 
 export function makeScrollToTopButton() {
+  const hiddenClassName = "scroll-to-top-button--hidden";
+
   const scrollButton = document.createElement("button");
   scrollButton.classList.add("scroll-to-top-button");
-  scrollButton.classList.add("hidden");
+  scrollButton.classList.add(hiddenClassName);
   scrollButton.innerHTML = `<span class="fas fa-arrow-alt-circle-up"></span>`;
   scrollButton.addEventListener("click", () => window.scrollTo(0, 0));
   document.body.append(scrollButton);
+
+  scrollButton.addEventListener("transitionend", function hideButton(event) {
+    if (this.classList.contains(hiddenClassName)) {
+      this.style.display = "none";
+    }
+  });
 
   requestAnimationFrame(function checkScroll() {
     const { innerHeight, scrollY } = window;
@@ -15,12 +23,16 @@ export function makeScrollToTopButton() {
     /** Bottom pixel of current scroll */
     const currentScrollBottom = innerHeight + scrollY;
 
-    if (scrollY < innerHeight / 3) {
-      scrollButton.classList.add("hidden");
-    } else if (scrollHeight - currentScrollBottom < 100) {
-      scrollButton.classList.add("hidden");
+    const isNearPageTop = scrollY < innerHeight / 3;
+    const isNearPageBottom = scrollHeight - currentScrollBottom < 100;
+
+    if (isNearPageTop || isNearPageBottom) {
+      scrollButton.classList.add(hiddenClassName);
     } else {
-      scrollButton.classList.remove("hidden");
+      scrollButton.style.removeProperty("display");
+      setTimeout(() => {
+        scrollButton.classList.remove(hiddenClassName);
+      }, 0);
     }
     requestAnimationFrame(checkScroll);
   });
